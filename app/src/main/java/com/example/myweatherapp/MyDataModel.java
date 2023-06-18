b45d8f81b621ebb6c3f0b72b56067e4eed5b7eb3
bajracharyaritika08 <ritikabajracharya415@gmail.com>
2023-06-18 21:20:00 +0545

    Weather app/ Finalized/ College Project/ Ritika

---
+++ app/src/main/java/com/example/myweatherapp/MyDataModel.java
@@ -0,0 +1,26 @@
+package com.example.myweatherapp;
+
+public class MyDataModel {
+    private String dt;
+    private String temp;
+    private String imageUrl;
+
+    public MyDataModel(String dt, String temp, String imageUrl) {
+        this.dt = dt;
+        this.temp = temp;
+        this.imageUrl = imageUrl;
+    }
+
+    public String getDt() {
+        return dt;
+    }
+
+    public String getTemp() {
+        return temp;
+    }
+
+    public String getImageUrl() {
+        return imageUrl;
+    }
+}
+
